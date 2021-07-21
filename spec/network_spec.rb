468eeb7ef3170c7e7206311a296b45622252d2e8
Tanner Dale <tanner@tannerdale.com>
2021-07-21 10:41:22 -0500

    Add iteration 4 tests

--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe Network do
     end
   end
 
+  # iteration 3
   context 'adding shows' do
     nbc = Network.new("NBC")
 
@@ -63,6 +64,39 @@ RSpec.describe Network do
         parks_and_rec => ["Amy Poehler", "Nick Offerman"]
       }
       expect(nbc.actors_by_show).to eq(expected)
+    end
+  end
+
+  # iteration 4
+  context 'iteration 4' do
+    nbc = Network.new("NBC")
+
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    mitch = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000})
+    baywatch = Show.new("Baywatch", "Gregory Bonann", [mitch])
+
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    nbc.add_show(knight_rider)
+    nbc.add_show(baywatch)
+    nbc.add_show(parks_and_rec)
+
+    it 'has shows by actors' do
+      expected = {
+        "David Hasselhoff" => [knight_rider, baywatch],
+        "William Daniels" => [knight_rider],
+        "Amy Poehler" => [parks_and_rec],
+        "Nick Offerman" => [parks_and_rec]
+      }
+    end
+
+    it 'has prolific actors' do
+      expect(nbc.prolific_actors).to eq(["David Hasselhoff"])
     end
   end
 end
