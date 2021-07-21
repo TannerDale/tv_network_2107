3208536514fc452566c6fc83998b297702bc2ca7
Tanner Dale <tanner@tannerdale.com>
2021-07-21 10:16:38 -0500

    Add show class

--- lib/show.rb
+++ lib/show.rb
@@ -1,1 +1,25 @@
+class Show
+  attr_reader :name, :creator, :characters
 
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    @characters.sum do |character|
+      character.salary
+    end
+  end
+
+  def highest_paid_actor
+    @characters.max_by { |character|
+      character.salary
+    }.actor
+  end
+
+  def actors
+    @characters.map { |character| character.actor }
+  end
+end
